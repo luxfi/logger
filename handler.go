78fb800468109d7d8229bd75d2c3c7bb001b5b70
Zach Kelling <z@zeekay.io>
2025-12-13 09:52:55 +0000

    Update handler for improved logging

--- handler.go
+++ handler.go
@@ -197,3 +197,148 @@ func builtinReplace(_ []string, attr slog.Attr, logfmt bool) slog.Attr {
 	}
 	return attr
 }
+
+// SlogBridgeLogger implements Logger interface using a slog.Handler
+// This bridges geth-style slog handlers to luxfi/log Logger interface
+type SlogBridgeLogger struct {
+	slogger *slog.Logger
+	handler slog.Handler
+	level   slog.Level
+}
+
+// NewLoggerFromHandler creates a Logger from a slog.Handler
+// This is used for geth compatibility where handlers implement slog.Handler
+func NewLoggerFromHandler(h slog.Handler) Logger {
+	return &SlogBridgeLogger{
+		slogger: slog.New(h),
+		handler: h,
+		level:   LevelInfo,
+	}
+}
+
+func (l *SlogBridgeLogger) With(ctx ...interface{}) Logger {
+	attrs := make([]slog.Attr, 0, len(ctx)/2)
+	for i := 0; i < len(ctx)-1; i += 2 {
+		key, ok := ctx[i].(string)
+		if !ok {
+			key = fmt.Sprintf("%v", ctx[i])
+		}
+		attrs = append(attrs, slog.Any(key, ctx[i+1]))
+	}
+	return &SlogBridgeLogger{
+		slogger: slog.New(l.handler.WithAttrs(attrs)),
+		handler: l.handler.WithAttrs(attrs),
+		level:   l.level,
+	}
+}
+
+func (l *SlogBridgeLogger) New(ctx ...interface{}) Logger {
+	return l.With(ctx...)
+}
+
+func (l *SlogBridgeLogger) Log(level slog.Level, msg string, ctx ...interface{}) {
+	l.WriteLog(level, msg, ctx...)
+}
+
+func (l *SlogBridgeLogger) Trace(msg string, ctx ...interface{}) {
+	l.WriteLog(LevelTrace, msg, ctx...)
+}
+
+func (l *SlogBridgeLogger) Debug(msg string, ctx ...interface{}) {
+	l.WriteLog(LevelDebug, msg, ctx...)
+}
+
+func (l *SlogBridgeLogger) Info(msg string, ctx ...interface{}) {
+	l.WriteLog(LevelInfo, msg, ctx...)
+}
+
+func (l *SlogBridgeLogger) Warn(msg string, ctx ...interface{}) {
+	l.WriteLog(LevelWarn, msg, ctx...)
+}
+
+func (l *SlogBridgeLogger) Error(msg string, ctx ...interface{}) {
+	l.WriteLog(LevelError, msg, ctx...)
+}
+
+func (l *SlogBridgeLogger) Crit(msg string, ctx ...interface{}) {
+	l.WriteLog(LevelCrit, msg, ctx...)
+}
+
+func (l *SlogBridgeLogger) WriteLog(level slog.Level, msg string, attrs ...any) {
+	slogAttrs := make([]slog.Attr, 0, len(attrs)/2)
+	for i := 0; i < len(attrs)-1; i += 2 {
+		key, ok := attrs[i].(string)
+		if !ok {
+			key = fmt.Sprintf("%v", attrs[i])
+		}
+		slogAttrs = append(slogAttrs, slog.Any(key, attrs[i+1]))
+	}
+	l.slogger.LogAttrs(context.Background(), level, msg, slogAttrs...)
+}
+
+func (l *SlogBridgeLogger) Enabled(ctx context.Context, level slog.Level) bool {
+	return l.handler.Enabled(ctx, level)
+}
+
+func (l *SlogBridgeLogger) Handler() slog.Handler {
+	return l.handler
+}
+
+func (l *SlogBridgeLogger) Fatal(msg string, fields ...Field) {
+	l.WriteLog(LevelFatal, msg)
+}
+
+func (l *SlogBridgeLogger) Verbo(msg string, fields ...Field) {
+	l.WriteLog(LevelVerbo, msg)
+}
+
+func (l *SlogBridgeLogger) WithFields(fields ...Field) Logger {
+	return l
+}
+
+func (l *SlogBridgeLogger) WithOptions(opts ...Option) Logger {
+	return l
+}
+
+func (l *SlogBridgeLogger) SetLevel(level slog.Level) {
+	l.level = level
+}
+
+func (l *SlogBridgeLogger) GetLevel() slog.Level {
+	return l.level
+}
+
+func (l *SlogBridgeLogger) EnabledLevel(lvl slog.Level) bool {
+	return l.handler.Enabled(context.Background(), lvl)
+}
+
+func (l *SlogBridgeLogger) StopOnPanic() {}
+
+func (l *SlogBridgeLogger) RecoverAndPanic(f func()) {
+	defer func() {
+		if r := recover(); r != nil {
+			panic(r)
+		}
+	}()
+	f()
+}
+
+func (l *SlogBridgeLogger) RecoverAndExit(f, exit func()) {
+	defer func() {
+		if r := recover(); r != nil {
+			exit()
+		}
+	}()
+	f()
+}
+
+func (l *SlogBridgeLogger) Stop() {}
+
+func (l *SlogBridgeLogger) Write(p []byte) (n int, err error) {
+	msg := string(p)
+	if len(msg) > 0 && msg[len(msg)-1] == '\n' {
+		msg = msg[:len(msg)-1]
+	}
+	l.Info(msg)
+	return len(p), nil
+}
